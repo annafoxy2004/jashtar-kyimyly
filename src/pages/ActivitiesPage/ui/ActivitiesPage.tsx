41661fbd478e3aed60d0de3999d3c1afcc852997
Nurzada <nurzadatobokelova@gmail.com>
2025-07-30 19:21:26 +0600

    added new two blocks

---
+++ src/pages/ActivitiesPage/ui/ActivitiesPage.tsx
@@ -0,0 +1,12 @@
+
+import ActivitiesSection from '@widgets/ActivitiesSection/ui/ActivitiesSection'
+
+function ActivitiesPage() {
+  return (
+    <div>
+      <ActivitiesSection />
+    </div>
+  )
+}
+
+export default ActivitiesPage
