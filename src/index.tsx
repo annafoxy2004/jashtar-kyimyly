b4c4d620103e2b7db8d2b8cb23c368362b9a5a48
Анна Рачкова <annarachkova@MacBook-Air-Anna.local>
2025-07-30 06:26:35 +0600

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <BrowserRouter>
-  <App />
-</BrowserRouter>);
+import "./app"; // просто импортируем всё из app/index.tsx
